79a586949b9715013e0ea8e7d8ef64a73ee6c05b
Alex4952 <keyzoo@gmail.com>
2015-07-26 11:54:53 +0900

    draft

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var pLabel: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		pLabel.text = "Hello World"
 	}
 
 	override func didReceiveMemoryWarning() {
